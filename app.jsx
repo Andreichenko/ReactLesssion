110ec8cf84747ebee36fefa448163d2b90af0194
aleksandrandreichenko <rsn_alex@mail.ru>
2017-12-15 16:20:27 +0300

    JSX Lesson

---
+++ app.jsx
@@ -0,0 +1,10 @@
+var jsx =
+
+    <div className="container">
+    <img src="https://facebook.github.io/img.svg"/>
+    <h1>React</h1>
+    <p>ReactJS is good</p>
+
+</div>;
+
+    ReactDOM.render(jsx, document.getElementById('root'));
